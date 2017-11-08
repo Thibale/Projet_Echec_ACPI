3883fa8c8b08054acfa4cfbafa411108b40aee15
Thibale <thib97@laposte.net>
2017-11-08 10:52:07 +0100

    Ajout gets sur Joueurs

--- projetEchec/src/projetechec/Joueurs.java
+++ projetEchec/src/projetechec/Joueurs.java
@@ -38,54 +38,83 @@ public class Joueurs {
     public int getIdJ(){
         return this.idJ;
     }
-    
     public String getNumLicenceJ(){
         return this.numLicenceJ;
     }
-    
     public String getNomJ(){
         return this.nomJ;
     }
-    
     public String getPrenomJ(){
         return this.prenomJ;
     }
-    
     public int getNumEloNormalJ(){
         return this.numEloNormalJ;
     }
-    
     public int getNumEloSemiRapideJ(){
         return this.numEloSemiRapideJ;
     }
-    
     public int getNumEloRapideJ(){
         return this.numEloRapideJ;
     }
-    
     public String getCategorieJ(){
         return this.categorieJ;
     }
-    
     public String getDateNaisJ(){
         return this.dateNaisJ;
     }
-    
     public char getSexeJ(){
         return this.sexeJ;
     }
-    
     public String getFederationJ(){
         return this.federationJ;
     }
-    
     public String getLigueJ(){
         return this.ligueJ;
     }
-    
     public String getClubJ(){
         return this.clubJ;
     }
+    
+    public void getIdJ(int id){
+        this.idJ = id;
+    }
+    public void getNumLicenceJ(String numLic){
+        this.numLicenceJ = numLic;
+    }
+    public void getNomJ(String nom){
+        this.nomJ = nom;
+    }
+    public void getPrenomJ(String prenom){
+        this.prenomJ = prenom;
+    }
+    public void getNumEloNormalJ(int elo){
+        this.numEloNormalJ = elo;
+    }
+    public void getNumEloSemiRapideJ(int elo){
+        this.numEloSemiRapideJ = elo;
+    }
+    public void getNumEloRapideJ(int elo){
+        this.numEloRapideJ = elo;
+    }
+    public void getCategorieJ(String cat){
+        this.categorieJ = cat;
+    }
+    public void getDateNaisJ(String date){
+        this.dateNaisJ = date;
+    }
+    public void getSexeJ(char sexe){
+        this.sexeJ = sexe;
+    }
+    public void getFederationJ(String fed){
+        this.federationJ = fed;
+    }
+    public void getLigueJ(String ligue){
+        this.ligueJ = ligue;
+    }
+    public void getClubJ(String club){
+        this.clubJ = club;
+    }
+    
 }
 
     
