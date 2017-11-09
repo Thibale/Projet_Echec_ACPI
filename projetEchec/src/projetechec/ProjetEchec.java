e38c043bc97c548dba54f8363eec0f91aa957c26
Thibale <thib97@laposte.net>
2017-11-09 10:51:24 +0100

    commit

--- projetEchec/src/projetechec/ProjetEchec.java
+++ projetEchec/src/projetechec/ProjetEchec.java
@@ -4,14 +4,9 @@ public class ProjetEchec {
     public static int numero = 0;
     
     public static void main(String[] args) {
-<<<<<<< HEAD
-       Joueurs j = new Joueurs(1,"294J","Falcone","Tibo",1234,5678,9101,"junM","01-01-1997","H","Fr","ligue","nimes");
-       Joueurs j2 = new Joueurs(2,"293J","Bert","Flo",1264,5698,3101,"junF","01-01-1997","F","Fr","ligue","nimes");
-=======
-       
        Joueurs j = new Joueurs(numero+=1,"294J","Falcone","Tibo",1234,5678,9101,"2006-01-01",'H',"Fr","ligue","nimes");
        Joueurs j2 = new Joueurs(numero+=1,"293J","Bert","Flo",1264,5698,3101,"1969-01-01",'F',"Fr","ligue","nimes");
->>>>>>> Thib1
+
        XML xml1 = new XML();
        xml1.WriteXML(j);
        xml1.WriteXML(j2);
